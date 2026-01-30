414ef85d84da95fca23dc7f990e2686503b99d25
Shatrox <kevinad.marques@gmail.com>
2026-01-30 16:03:07 +0100

    update migration

--- Demo_WebAPI_EventAgenda.Infrastructure.Database/Migrations/20260130150120_Fix Desc 2.Designer.cs
+++ Demo_WebAPI_EventAgenda.Infrastructure.Database/Migrations/20260130150120_Fix Desc 2.Designer.cs
@@ -4,6 +4,7 @@ using Demo_WebAPI_EventAgenda.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Demo_WebAPI_EventAgenda.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260130150120_Fix Desc 2")]
+    partial class FixDesc2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Demo_WebAPI_EventAgenda.Infrastructure.Database/Migrations/20260130150120_Fix Desc 2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo_WebAPI_EventAgenda.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDesc2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Agenda_Events",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Agenda_Events",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
