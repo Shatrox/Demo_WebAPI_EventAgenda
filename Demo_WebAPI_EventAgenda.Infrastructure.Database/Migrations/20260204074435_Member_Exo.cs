0066c885dcfd9668f5e9d782639931a82fd64b6e
Shatrox <kevinad.marques@gmail.com>
2026-02-04 08:53:33 +0100

    migartion members

---
+++ Demo_WebAPI_EventAgenda.Infrastructure.Database/Migrations/20260204074435_Member_Exo.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo_WebAPI_EventAgenda.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Member_Exo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Pseudonyme = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "varchar(320)", unicode: false, maxLength: 320, nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    AllowNewsletter = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_Members__Email",
+                table: "Members",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_Members_Pseudonyme",
+                table: "Members",
+                column: "Pseudonyme",
+                unique: true,
+                filter: "[Pseudonyme] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
